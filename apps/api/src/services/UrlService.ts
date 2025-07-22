5ae2ecc0b69e684cd3a237423d2f5de073978d1d
위성원 <tkrhkaos12@naver.com>
2025-07-23 00:40:48 +0900

    feat: add service

--- apps/api/src/services/UrlService.ts
+++ apps/api/src/services/UrlService.ts
@@ -8,23 +8,18 @@ class UrlService {
   async createShortUrl(originalUrl: string): Promise<string> {
     for (let i = 0; i < this.MAX_RETRIES; i++) {
       try {
-        // 1. URL 정보 삽입 (ID 자동 생성)
         const newId = await UrlRepository.create(originalUrl);
 
-        // 2. Base62 인코딩
         const base62EncodedId = encode(newId);
 
-        // 3. 2자리의 랜덤 문자열 추가 (앞뒤 1자리씩)
         const randomPrefix = getRandomBase62Char();
         const randomSuffix = getRandomBase62Char();
 
-        // 최종 단축키 조합
         const shortKey = `${randomPrefix}${base62EncodedId}${randomSuffix}`;
 
-        // 4. 생성된 ID에 단축키 업데이트 (Unique Index가 여기서 작동)
         await UrlRepository.updateShortKey(newId, shortKey);
 
-        return shortKey; // 성공적으로 생성된 단축키 반환
+        return shortKey;
       } catch (error: any) {
         // PostgreSQL Unique Constraint Violation 에러 (error.code '23505')
         if (error.code === '23505') {
