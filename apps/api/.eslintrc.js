7b7382e046e4f176772db0f34742cfe8ab2c215b
위성원 <tkrhkaos12@naver.com>
2025-07-22 22:16:51 +0900

    feat(api): fix typescript interface reserved error

--- apps/api/.eslintrc.js
+++ apps/api/.eslintrc.js
@@ -2,4 +2,11 @@
 module.exports = {
   root: true,
   extends: ["@repo/eslint-config/server.js"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: "./tsconfig.json",
+  },
 };
--- packages/eslint-config/server.js
+++ packages/eslint-config/server.js
@@ -1,5 +1,7 @@
 module.exports = {
-  extends: ["eslint:recommended"],
+  extends: ["eslint:recommended", "@typescript-eslint/recommended"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
   env: {
     node: true,
     es6: true,
@@ -8,11 +10,22 @@ module.exports = {
     ecmaVersion: "latest",
     sourceType: "module",
   },
+  rules: {
+    // TypeScript 관련 규칙 비활성화 (필요에 따라 조정)
+    "@typescript-eslint/no-unused-vars": "warn",
+    "@typescript-eslint/no-explicit-any": "warn",
+  },
   overrides: [
     {
       files: ["**/__tests__/**/*"],
       env: {
         jest: true,
+      },
+    },
+    {
+      files: ["*.js"],
+      rules: {
+        "@typescript-eslint/no-var-requires": "off",
       },
     },
   ],
