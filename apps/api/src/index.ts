8d63b05eea26ad587167929dc2f015cdff4105a3
위성원 <tkrhkaos12@naver.com>
2025-07-24 00:29:53 +0900

    feat: rm unused code

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,7 +1,6 @@
-import db from "./db";
+import { log } from "@repo/logger";
 import urlRoutes from "./routes/urlRoutes";
 import { createServer } from "./server";
-import { log } from "@repo/logger";
 
 const port = process.env.PORT || 3001;
 const server = createServer();
